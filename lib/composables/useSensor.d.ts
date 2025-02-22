2eaf2b74b7e3cf42cc851db2d72e2387df2c33e2
ZiZIGY <zizigydev@gmail.com>
2025-02-23 01:23:47 +0300

    remove lib folder

--- lib/composables/useSensor.d.ts
+++
@@ -1,6 +0,0 @@
-import { Ref } from 'vue';
-export declare const useSensor: (elementRef: Ref<HTMLElement | null>) => {
-    activate: (event: PointerEvent) => void;
-    track: (event: PointerEvent | WheelEvent) => void;
-    deactivate: () => void;
-};
