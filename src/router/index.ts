270d4e229991e81f307aa354da2a2c253685e853
ZiZIGY <zizigydev@gmail.com>
2025-02-23 06:28:31 +0300

    fix router and path

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
